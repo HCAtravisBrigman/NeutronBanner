76264e48599e6df309d72510b2365ea9318c3037
Travis Brigman <travis.brigman@hcahealthcare.com>
2021-11-01 14:44:22 -0500

    Initial Commit

---
+++ NeutronBanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeutronBanner
+//
+//  Created by UXCOE Floater on 11/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NeutronBanner/NeutronBannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeutronBannerApp.swift
+//  NeutronBanner
+//
+//  Created by UXCOE Floater on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct NeutronBannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
